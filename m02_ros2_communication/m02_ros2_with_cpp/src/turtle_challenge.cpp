d62759b7ee699a6803725b93e1ff3fd195169961
DanielFLopez1620 <dfelipe.lopez@gmail.com>
2024-04-03 08:57:33 -0500

    M02: Turtle challenge Teleport relateve and setpen, also main with rclcpp

--- m02_ros2_communication/m02_ros2_with_cpp/src/turtle_challenge.cpp
+++ m02_ros2_communication/m02_ros2_with_cpp/src/turtle_challenge.cpp
@@ -64,7 +64,7 @@ public:
         while(!this->kill_client_->wait_for_service(1s))
         {
             RCLCPP_INFO(rclcpp::get_logger("rclcpp"), 
-                "Waiting for service in turtlesim: Spawn");
+                "Waiting for service in turtlesim: Kill");
         }
         auto result = kill_client_->async_send_request(request);
         if(rclcpp::spin_until_future_complete(this->get_node_base_interface(), result) == 
@@ -87,9 +87,57 @@ public:
         while(!this->tp_abs_client_->wait_for_service(1s))
         {
             RCLCPP_INFO(rclcpp::get_logger("rclcpp"), 
-                "Waiting for service in turtlesim: Spawn");
+                "Waiting for service in turtlesim: Teleport Absolute");
         }
         auto result = tp_abs_client_->async_send_request(request);
+        if(rclcpp::spin_until_future_complete(this->get_node_base_interface(), result) == 
+            rclcpp::FutureReturnCode::SUCCESS)
+        {
+            return true;
+        }
+        return false;
+    }
+
+    bool teleport_rel_turtle(float linear, float angular, std::string& name)
+    {
+        tp_rel_client_ = this->create_client<turtlesim::srv::TeleportRelative>
+            ( "/" + name + "/teleport_relative");
+        auto request = 
+            std::make_shared<turtlesim::srv::TeleportRelative::Request>();
+        request->linear = linear;
+        request->angular = angular;
+        while(!this->tp_rel_client_->wait_for_service(1s))
+        {
+            RCLCPP_INFO(rclcpp::get_logger("rclcpp"), 
+                "Waiting for service in turtlesim: Teleport Relative");
+        }
+        auto result = tp_rel_client_->async_send_request(request);
+        if(rclcpp::spin_until_future_complete(this->get_node_base_interface(), result) == 
+            rclcpp::FutureReturnCode::SUCCESS)
+        {
+            return true;
+        }
+        return false;
+    }
+
+    bool set_pen_turtle(unsigned int r,unsigned int g, unsigned int b, 
+        unsigned int width, std::string& name)
+    {
+        pen_client_ = this->create_client<turtlesim::srv::SetPen>
+            ( "/" + name + "/set_pen");
+        auto request = 
+            std::make_shared<turtlesim::srv::SetPen::Request>();
+        request->r = r;
+        request->g = g;
+        request->b = b;
+        request->width = width;
+        request->off = 0;
+        while(!this->pen_client_->wait_for_service(1s))
+        {
+            RCLCPP_INFO(rclcpp::get_logger("rclcpp"), 
+                "Waiting for service in turtlesim: SetPen");
+        }
+        auto result = pen_client_->async_send_request(request);
         if(rclcpp::spin_until_future_complete(this->get_node_base_interface(), result) == 
             rclcpp::FutureReturnCode::SUCCESS)
         {
@@ -110,3 +158,25 @@ private:
     rclcpp::Client<turtlesim::srv::SetPen>::SharedPtr pen_client_;
     rclcpp::Subscription<turtlesim::msg::Pose>::SharedPtr pose_subscriber_;
 };
+
+int main(int argc, char* argv[])
+{
+    rclcpp::init(argc, argv);
+
+    std::string name1 {"dan_turtle"};
+    auto ptr_play = std::make_shared<PlaygroundTurtle>();
+    ptr_play->clear_board();
+    ptr_play->spawn_turtle(3, 3, 0, name1);
+    ptr_play->teleport_abs_turtle(7, 3, 3*M_PI_2, name1);
+    ptr_play->set_pen_turtle(255, 0, 0, 2, name1);
+    ptr_play->teleport_abs_turtle(7, 7, -M_PI, name1);
+    ptr_play->set_pen_turtle(0, 255, 0, 2, name1);
+    ptr_play->teleport_abs_turtle(3, 7, M_PI_2, name1);
+    ptr_play->set_pen_turtle(0, 0, 255, 2, name1);
+    ptr_play->teleport_abs_turtle(3, 3, M_PI_2, name1);
+    ptr_play->teleport_rel_turtle(1, M_PI_2, name1);
+    ptr_play->kill_turtle(name1);
+    
+    rclcpp::shutdown();
+    return 0;
+}
