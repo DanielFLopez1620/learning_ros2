0297b39dbccb8ee93c8611925f123b46bbcbc190
DanielFLopez1620 <dfelipe.lopez@gmail.com>
2024-04-28 08:30:16 -0500

    M02: Base figure plugins comments

--- m02_ros2_communication/m02_base_figure/include/m02_base_figure/base_figure.hpp
+++ m02_ros2_communication/m02_base_figure/include/m02_base_figure/base_figure.hpp
@@ -1,17 +1,25 @@
+// Ensure that is not defined multiple times
 #ifndef M02_BASE_FIGURE_BASE_FIGURE_HPP
 #define M02_BASE_FIGURE_BASE_FIGURE_HPP
 
 namespace m02_base_figure
 {
+    // Parent class of the plugin
     class BaseFigure
     {
         public:
-        virtual void initialize(double side_length) = 0;
-        virtual double area() = 0;
-        virtual ~BaseFigure(){}
+            // Initialize most valuable size, must create impl. with child
+            virtual void initialize(double side_length) = 0;
+
+            // Calculate area, must create implementation with child
+            virtual double area() = 0;
+
+            // Destructor
+            virtual ~BaseFigure(){}
 
         protected:
-        BaseFigure(){}
+            // Protected constructor as it will be used with inheritance
+            BaseFigure(){}
     };
 }  // namespace m02_base_figure
 
--- m02_ros2_communication/m02_base_figure/src/area_calculator.cpp
+++ m02_ros2_communication/m02_base_figure/src/area_calculator.cpp
@@ -1,25 +1,38 @@
-#include <pluginlib/class_loader.hpp>
-#include <m02_base_figure/base_figure.hpp>
+// ------------------------ C++ STANDARD LIBRARIES -----------------------------
+#include <iostream>
 
+// ---------------------- PLUGINLIBS HEADERS -----------------------------------
+#include <pluginlib/class_loader.hpp>        // Loader required
+
+// ---------------------- PLUGINS DEFINITIONS ----------------------------------
+#include <m02_base_figure/base_figure.hpp>   // Base class inclussion
+
+// --------------------- MAIN IMPLEMENTATION -----------------------------------
 int main(int argc, char** argv)
 {
-  // To avoid unused parameter warnings
-  (void) argc;
-  (void) argv;
+    // Load base class present in header file
+    pluginlib::ClassLoader<m02_base_figure::BaseFigure> 
+        figure_loader("m02_base_figure", "m02_base_figure::BaseFigure");
 
-  pluginlib::ClassLoader<m02_base_figure::BaseFigure> figure_loader("m02_base_figure", "m02_base_figure::BaseFigure");
+    // Use plugin under test environment
+    try
+    {
+        // Access to the plugin info to instance a circle object.
+        std::shared_ptr<m02_base_figure::BaseFigure> circle = 
+            figure_loader.createSharedInstance("m02_figure_plugins::Circle");
+        
+        // Set main dimension (diameter in this case)
+        circle->initialize(2.0);
 
-  try
-  {
-    std::shared_ptr<m02_base_figure::BaseFigure> circle = figure_loader.createSharedInstance("m02_figure_plugins::Circle");
-    circle->initialize(2.0);
+        // Use area method and display the result
+        std::cout << "Circle area: " <<  circle->area()); << std::endl;
+    }
+    catch(pluginlib::PluginlibException& ex)
+    {
+        // Show message in case of result not being displayed.
+        std::cout << "The plugin failed to load for some reason. Error\n: "
+                  << ex.what() << std::endl;
+    }
 
-    printf("Circle area: %.2f\n", circle->area());
-  }
-  catch(pluginlib::PluginlibException& ex)
-  {
-    printf("The plugin failed to load for some reason. Error: %s\n", ex.what());
-  }
-
-  return 0;
+    return 0;
 }
