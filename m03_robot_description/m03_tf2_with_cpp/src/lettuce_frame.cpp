a9df381b2353107a2fb62a4b14692eb2a4d08f5e
DanielFLopez1620 <dfelipe.lopez@gmail.com>
2024-08-23 11:07:09 -0500

    M03: Comments on tf2 with cpp P2

--- m03_robot_description/m03_tf2_with_cpp/src/lettuce_frame.cpp
+++ m03_robot_description/m03_tf2_with_cpp/src/lettuce_frame.cpp
@@ -1,16 +1,34 @@
-#include <chrono>
-#include <functional>
-#include <memory>
+// ---------------------- STANDARD HEADERS REQUIRED ---------------------------
+#include <chrono>      // Time management with different precisions
+#include <functional>  // For function usage and hash related
+#include <memory>      // Dynamic memory management
 
-#include "rclcpp/rclcpp.hpp"
-#include "tf2_ros/transform_broadcaster.h"
-#include "geometry_msgs/msg/transform_stamped.hpp"
+// ----------------------- ROS2 RELATED HEADERS -------------------------------
+#include "rclcpp/rclcpp.hpp"                 // ROS2 Client Library for C++
+#include "tf2_ros/transform_broadcaster.h"   // Tf broadcaster
 
-using namespace std::chrono_literals;
+// ------------------------ ROS2 MESSAGES -------------------------------------
+#include "geometry_msgs/msg/transform_stamped.hpp"  // Tf with time stamp
 
+// ------------------------ NAMESPACE CONSIDERATIONS --------------------------
+using namespace std::chrono_literals;  // For user-defined chrono literals
+
+
+// --------------------- FRAME BROADCASTER IMPLEMENTATION --------------------
+
+/**
+ * Class that publishes a static frame from the turtle, you can take it as the 
+ * fishing rot with carrot example, that always stays at the same position from
+ * the turtle, like in Minecraft with pigs.
+ */
 class LettuceFrameBroadcaster : public rclcpp::Node
 {
 public:
+    /**
+     * User defined constructor that initialize the node under the name 
+     * "fixed_lettuce_tf2_broadcaster", declare a broadcaster and a timer
+     * linked with a callback.
+     */
     LettuceFrameBroadcaster() 
         : Node("fixed_lettuce_tf2_broadcaster")
     {
@@ -20,34 +38,51 @@ public:
     }
 
 private:
+    /**
+     * Callback that takes place when a timer specifies it, to broadcast a 
+     * transform that stays fixed relative to the turtle1
+     */
     void broad_callback()
     {
+        // Declare stamped transform
         geometry_msgs::msg::TransformStamped t_stamp;
 
+        // Add header information: Time, parent and child
         t_stamp.header.stamp = this->get_clock()->now();
         t_stamp.header.frame_id = "turtle1";
         t_stamp.child_frame_id = "lettuce";
         
+        // Constant traslational values
         t_stamp.transform.translation.x = 0.0;
         t_stamp.transform.translation.y = 2.0;
         t_stamp.transform.translation.z = 0.0;
 
+        // Constant rotational values (Quaternion)
         t_stamp.transform.rotation.x = 0.0;
         t_stamp.transform.rotation.y = 0.0;
         t_stamp.transform.rotation.z = 0.0;
         t_stamp.transform.rotation.w = 1.0;
 
+        // Publish transform
         tf_broad_->sendTransform(t_stamp);
     }
 
+    // Declare timer and transform broadcaster
     rclcpp::TimerBase::SharedPtr timer_;
     std::shared_ptr<tf2_ros::TransformBroadcaster> tf_broad_;
 };
 
+// -------------------- MAIN IMPLEMENTATION -----------------------------------
+
 int main(int argc, char * argv[])
 {
+    // Initialize ROS2 Client Library for C++
     rclcpp::init(argc, argv);
+
+    // Spin node creation
     rclcpp::spin(std::make_shared<LettuceFrameBroadcaster>());
+    
+    // When spin is interrupted, close and shutdown
     rclcpp::shutdown();
     return 0;
 }
