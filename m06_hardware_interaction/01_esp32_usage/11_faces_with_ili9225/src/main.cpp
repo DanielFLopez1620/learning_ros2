3ec36f4799e64d02911e1a5d2ecd19c0fdd54a06
DanielFLopez1620 <dfelipe.lopez@gmail.com>
2025-03-27 13:03:57 -0500

    Hardware integration: Faces with ILI9225 (No ROS)

--- m06_hardware_interaction/01_esp32_usage/11_faces_with_ili9225/src/main.cpp
+++ m06_hardware_interaction/01_esp32_usage/11_faces_with_ili9225/src/main.cpp
@@ -22,11 +22,13 @@
 #define TFT_CLK        14   // SCK pin
 #define TFT_LED        0    // Should be 0 if wired to +5V
 #define TFT_BRIGHTNESS 200  // Brightness level
-#define X_INI 40    // X begin for base eyes
-#define X_END 155   // X end for base eyes
+#define X_INI 45    // X begin for base eyes
+#define X_END 125   // X end for base eyes
+#define X_OFF 30    // X Offset planned
 #define Y1_INI 20   // Y first eye begin for base eye
 #define Y2_INI 130  // Y second eye begin for base eye
 #define Y_SIZE 70   // Y size of eyes
+#define TIME_OUT 2500 // Time out
 
 // Screen definitions
 TFT_22_ILI9225 tft = TFT_22_ILI9225(TFT_RST, TFT_RS, TFT_CS, TFT_SDI, TFT_CLK,
@@ -47,10 +49,59 @@ void setup()
 // ------------------------- Loop implementation ---------------------------
 void loop() 
 {
+    // Surprise
+    tft.fillCircle((X_END + X_INI)/2, Y1_INI + Y_SIZE/2, Y_SIZE/2, COLOR_YELLOW);
+    tft.fillCircle((X_END + X_INI)/2, Y2_INI + Y_SIZE/2, Y_SIZE/2, COLOR_YELLOW);
+    delay(TIME_OUT);
+
+    tft.clear();
+    delay(100);
+
+    // Inexpressive
+    tft.fillRectangle(X_INI + X_OFF, Y1_INI, X_END - X_OFF, Y1_INI + Y_SIZE, COLOR_YELLOW);
+    tft.fillRectangle(X_INI + X_OFF, Y2_INI, X_END - X_OFF, Y2_INI + Y_SIZE, COLOR_YELLOW);
+    delay(TIME_OUT);
+
+    tft.clear();
+    delay(100);
+
+    // Angry
+    tft.fillRectangle(X_INI, Y1_INI, X_END, Y1_INI + Y_SIZE, COLOR_ORANGE);
+    tft.fillRectangle(X_INI, Y2_INI, X_END, Y2_INI + Y_SIZE, COLOR_ORANGE);
+    tft.fillTriangle(X_END, Y1_INI, X_END + 12, Y1_INI, X_END, Y1_INI + Y_SIZE, COLOR_ORANGE);
+    tft.fillTriangle(X_END, Y2_INI, X_END, Y2_INI + Y_SIZE, X_END + 12, Y2_INI + Y_SIZE, COLOR_ORANGE);
+    delay(TIME_OUT);
+
+    tft.clear();
+    delay(100);
+
+    // Sad
+    tft.fillRectangle(X_INI, Y1_INI, X_END, Y1_INI + Y_SIZE, COLOR_LIGHTBLUE);
+    tft.fillRectangle(X_INI, Y2_INI, X_END, Y2_INI + Y_SIZE, COLOR_LIGHTBLUE);
+    tft.fillTriangle(X_END, Y1_INI, X_END, Y1_INI + Y_SIZE, X_END + 10, Y1_INI + Y_SIZE, COLOR_LIGHTBLUE);
+    tft.fillTriangle(X_END, Y2_INI, X_END + 10, Y2_INI, X_END, Y2_INI + Y_SIZE, COLOR_LIGHTBLUE);
+    delay(TIME_OUT);
+
+    tft.clear();
+    delay(100);
+    
+    // Happy
     tft.fillRectangle(X_INI, Y1_INI, X_END, Y1_INI + Y_SIZE, COLOR_YELLOW);
     tft.fillRectangle(X_INI, Y2_INI, X_END, Y2_INI + Y_SIZE, COLOR_YELLOW);
-    delay(5000);
-    tft.fillRectangle(X_INI, Y1_INI, X_END, Y1_INI + Y_SIZE, COLOR_NAVY);
-    tft.fillRectangle(X_INI, Y2_INI, X_END, Y2_INI + Y_SIZE, COLOR_NAVY);
-    delay(5000);
+    tft.fillCircle(X_INI, Y1_INI + Y_SIZE/2, Y_SIZE/2, COLOR_YELLOW);
+    tft.fillCircle(X_END, Y1_INI + Y_SIZE/2, Y_SIZE/2, COLOR_YELLOW);
+    tft.fillCircle(X_INI, Y2_INI + Y_SIZE/2, Y_SIZE/2, COLOR_YELLOW);
+    tft.fillCircle(X_END, Y2_INI + Y_SIZE/2, Y_SIZE/2, COLOR_YELLOW);
+    delay(TIME_OUT);
+
+    tft.clear();
+    delay(100);
+    
+    // Neutral
+    tft.fillRectangle(X_INI, Y1_INI, X_END, Y1_INI + Y_SIZE, COLOR_YELLOW);
+    tft.fillRectangle(X_INI, Y2_INI, X_END, Y2_INI + Y_SIZE, COLOR_YELLOW);
+    delay(TIME_OUT);
+
+    tft.clear();
+    delay(100);
 }
